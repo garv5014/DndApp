842811ed01022dd23ef1081b4dda64b15ab22fe8
garv5014 <garv5014@gmail.com>
2022-11-10 12:57:34 -0700

    Getting Closer with the sqlite and made the character Creator page

--- DndApp/Data/UserCharacterInfo.cs
+++ DndApp/Data/UserCharacterInfo.cs
@@ -1,20 +1,30 @@
-﻿namespace DndApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DndApp.Data
 {
     public class UserCharacterInfo
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public string? Description { get; set; }
-
+        [Required]
         public string Class { get; set; }
+        [Required]
         public string Race { get; set; }
         public int? Age { get; set; }
+        [Required, Range(3,18)]
         public int Charisma { get; set; }
+        [Required, Range(3,18)]
         public int Intelligence { get; set; }
+        [Required, Range(3,18)]
         public int Strength { get; set; }
+        [Required, Range(3, 18)]
         public int Dexterity { get; set; }
+        [Required, Range(3, 18)]
         public int Constitution { get; set; }
+        [Required, Range(3, 18)]
         public int Wisdom { get; set; }
     }
 }
--- DndApp/Program.cs
+++ DndApp/Program.cs
@@ -2,6 +2,7 @@ using DndApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Diagnostics.CodeAnalysis;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,7 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddDbContextFactory<DNDDbContext>(options =>
     options.UseSqlite("data source=DND.db")
     .EnableSensitiveDataLogging());
