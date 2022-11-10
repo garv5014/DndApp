05fabf9339250c64aa21f6c32905a1933b077d3a
garv5014 <garv5014@gmail.com>
2022-11-09 20:14:26 -0700

    moved solutions from project

--- DndApp/Program.cs
+++ DndApp/Program.cs
@@ -9,13 +9,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddDbContext<DNDDbContext>(options =>
-    options.UseSqlite("Data Source = DNDServer"));
+builder.Services.AddDbContextFactory<DNDDbContext>(options =>
+    options.UseSqlite("Data Source = DND.db")
+    .EnableSensitiveDataLogging());
 
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
     options.ProviderOptions.LoginMode = "Redirect";
 });
-
 await builder.Build().RunAsync();
