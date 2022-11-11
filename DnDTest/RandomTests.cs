c1ac3e4b4d5d2ef6944a3717109ee3bcf0374a91
Stormgate1998 <jalapenojosh@gmail.com>
2022-11-11 13:33:46 -0700

    Got tests working

---
+++ DnDTest/RandomTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DndApp.Pages;
+namespace DnDTest
+{
+    public class RandomTests
+    {
+        [Fact]
+        public void Renders()
+        {
+            using var ctx = new TestContext();
+
+            // Render Counter component.
+            var component = ctx.RenderComponent<DiceRoller>();
+
+            // Assert: first, find the parent_name vital element, then verify its content.
+            Assert.Equal("Roll", component.Find($".btn").TextContent);
+
+
+        }
+        [Fact]
+        public void RandomNumber() {
+            using var ctx = new TestContext();
+
+            // Render Counter component.
+            var component = ctx.RenderComponent<DiceRoller>();
+        }
+    }
+}
