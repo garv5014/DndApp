1a6cb7ef159984b2bce91c124a35efac88dcaa9a
Stormgate1998 <jalapenojosh@gmail.com>
2022-11-10 21:09:22 -0700

    Made a broken bunit, added dice roller

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics.Metrics;
+using Xunit;
+using Bunit;
+using DndApp.Pages;
+namespace TestProject1
+{
+    public class RandomTester
+    {
+        [Fact]
+        public void Renders()
+        {
+            using var ctx = new TestContext();
+
+            // Render Counter component.
+            var component = ctx.RenderComponent<DiceRoller>();
+
+            // Assert: first, find the parent_name vital element, then verify its content.
+            Assert.Equal("Roll", component.Find($".btn").TextContent);
+
+
+        }
+    }
+}
